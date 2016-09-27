94a19174582a0185ee1b082de168470bda05cd9b
Ihor Kovalyshyn <ihorko.kovalyshyn@gmail.com>
2016-09-27 11:56:24 +0300

    change print functions

--- R/print_functions.R
+++ R/print_functions.R
@@ -1,76 +1,65 @@
-#' Print log message
-#'
-#' Prints a log to console or writes a log to a file
-#'
-#' @param msg A string
-#' @param time Time of the log to be printed
-#' @param logger_name A string
-#' @param file A connection, or a character string naming the file to print to.
-#'
-print_log_message <- function (msg, time, level = NULL, logger_name = NULL, file = '') {
-    if (!is.null(logger_name)) {
-        logger_name <- paste0('[', logger_name, ']')
-    }
-
-    if (file == '' & get_logging_file() != '') file <- get_logging_file()
-
-    if (compare_level(level = level)) {
-        cat(as.character(time), logger_name, level, paste0('[', msg, ']'),
-            sep = ' - ', fill = TRUE, file = file,
-            append = ifelse(file == '', FALSE, TRUE)
-            )
-    }
-}
-
-#' Print logtime message
-#'
-#' Prints a logtime message to console or writes a log to a file
+#' Get log
 #'
 #' @param msg A string
 #' @param time Time of the log to be printed
 #' @param file Path to a file
-#' @param start_or_end Wheter 'start' or 'end'
+#' @param start_or_end Wheter NULL, 'Start' or 'End'
 #' @param exec_time_sec Duration of code execution in seconds
 #' @param indentation_level An integer
 #' @param logger_name A string
-#' @param file A connection, or a character string naming the file to print to.
-print_logtime_message <- function (msg,
-                                   time,
-                                   start_or_end = 'start',
-                                   exec_time_sec = NULL,
-                                   indentation_level = 0,
-                                   level = NULL,
-                                   logger_name = NULL,
-                                   file = ''
-                                   ) {
-    stopifnot(start_or_end %in% c('start', 'end'))
+#'
+#' @return A list
+get_log <- function (msg,
+                     time,
+                     start_or_end = NULL,
+                     exec_time_sec = NULL,
+                     indentation_level = 0,
+                     level = NULL,
+                     logger_name = NULL,
+                     file = ''
+                     ) {
+     format_log_element <- function (element) {
+         if (!is.null(element)) paste0('[', element, ']')
+         else NULL
+     }
 
-    indent_string <- paste(rep(' ', indentation_level), collapse = ' ')
-    indentation_and_time <- paste0(indent_string, as.character(time))
+     # initialize log
+     log <-
+        list(time = NULL,
+             logger_name = format_log_element(logger_name),
+             level = level,
+             start_or_end = format_log_element(start_or_end),
+             msg = format_log_element(msg),
+             exec_time_msg = NULL
+             )
 
-    msg <- paste0('[', msg, ']')
+     # add indentation to time element
+     indent_string <- paste(rep(' ', indentation_level), collapse = ' ')
+     log$time <- paste0(indent_string, as.character(time))
 
-    if (start_or_end == 'start') {
-        start_or_end <- '[Start]'
-        exec_time_msg <- NULL
-    } else {
-        start_or_end <- '[End]'
-        exec_time_min <- round(exec_time_sec / 60, 1)
-
-        # execution time message
-        exec_time_msg <-
+     if (!is.null(start_or_end)) {
+        if (start_or_end == 'End') {
+            # execution time message
+            log$exec_time_msg <-
             paste0('[Done in ', round(exec_time_sec, 1),
-                   ' sec. ', '(', exec_time_min, ' min.',')', ']'
-                  )
-    }
+            ' sec. ', '(', round(exec_time_sec / 60, 1), ' min.',')', ']'
+            )
+        }
+     }
 
-    if (!is.null(logger_name)) logger_name <- paste0('[', logger_name, ']')
+     return(log)
+}
 
-    # write to file from global config if it is not empty and local file is empty
+
+#' Print log
+#'
+#' @param log A list
+#' @param file A connection, or a character string naming the file to print to
+print_log <- function (log, file = '', logger_name = NULL) {
     if (file == '' & get_logging_file() != '') file <- get_logging_file()
 
-    if (compare_level(level = level)) {
-        cat(indentation_and_time, logger_name, level, start_or_end, msg, exec_time_msg,
+    if (compare_level(level = log$level)) {
+        cat(unlist(log),
             sep = ' - ', fill = TRUE, file = file,
             append = ifelse(file == '', FALSE, TRUE)
             )
